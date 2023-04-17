6c05eceafa7da6d996c51065a8500b7f372ca786
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-04-17 16:33:34 -0500

    Inside Training Project and today I worked: Adding more things in this Clean Architecture Project

--- lib/app/pages/rounder_app.dart
+++ lib/app/pages/rounder_app.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:inside_training/app/pages/login/login_view.dart';
+import 'package:inside_training/app/pages/pages.dart';
+import 'package:inside_training/app/pages/splash/splash_view.dart';
 
-// class RouterApp {
-//   final RouterObserver<PageRoute> routerObserver;
-//   RouterApp(): routeObserver = RouterObserver<PageRoute>();
+class RouterApp {
+  final RouteObserver<PageRoute> routeObserver;
+  RouterApp() : routeObserver = RouteObserver<PageRoute>();
 
-// }
+  Route<dynamic> getRoute(RouteSettings settings) {
+    final Map args = settings.arguments as Map;
+
+    switch (settings.name) {
+      case Pages.splash:
+          return _buildRoute(settings, const SplashPage());
+        break;
+    }
+    return _buildRoute(settings, const LoginPage());
+  }
+
+  MaterialPageRoute<dynamic> _buildRoute(RouteSettings settings, Widget builder){
+    return MaterialPageRoute(
+      settings: settings,
+      builder: (BuildContext context) => builder
+    );
+  }
+
+}
